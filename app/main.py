ad7df0bbb93a0dbba14966129130c716b8a5eded
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2023-03-31 16:52:01 +0200

    Fix user_id for the latest changes

--- app/main.py
+++ app/main.py
@@ -38,7 +38,8 @@ async def get_current_user(
     x_pomerium_jwt_assertion: Optional[str] = Header(None),
 ):
     user_auth_data = jwt.JWT.get_current_user_info(x_pomerium_jwt_assertion)
-    user_info = crud.get_user_info(db, user_auth_data["sub"])
+    user_id = f"google/{user_auth_data['sub']}]}"
+    user_info = crud.get_user_info(db, user_id)
     if user_info is None:
         logger.error(f"No user info available for user {user_auth_data['sub']}")
         user_info = {}
